9fa6a3006fdb2e748069cd6c362ba35af532f4ef
Andreas Würl <andi@tryb.de>
2015-07-17 10:57:10 +0200

    updated version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='citools',
-    version='0.1.4',
+    version='0.1.5',
     packages=find_packages(),
     scripts=glob.glob('scripts/*'),
     description='CI Tools',
