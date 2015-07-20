cb5d3861918a1d608666676b764a7f3d1f413794
Andreas Würl <andi@tryb.de>
2015-07-20 17:49:56 +0200

    cleanup output

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='citools',
-    version='0.1.55',
+    version='0.1.57',
     packages=find_packages(),
     scripts=glob.glob('scripts/*'),
     description='CI Tools',
