1808e6d75d19cdf09ae53916bdfe258181a6e8f2
Andreas Würl <andi@tryb.de>
2015-07-21 23:39:00 +0200

    fixed tests

--- test/persistence/test_persistence.py
+++ test/persistence/test_persistence.py
@@ -1,6 +1,7 @@
 import os
 import tempfile
 from unittest import TestCase
+from assertpy import assert_that
 from citools.persistence import Persistence
 
 
@@ -9,7 +10,22 @@ class PersistenceTest(TestCase):
         output = tempfile.mkdtemp(prefix='persistence_test')
         persistence_file_name = os.path.join(output, 'name')
         with Persistence(persistence_file_name) as persistence:
-            report = persistence.get_report()
+            report = persistence.report
+
+        assert_that(report).is_none()
+
+        report_object = "foo"
+
+        with Persistence(persistence_file_name) as persistence:
+            persistence.report = report_object
+
+        with Persistence(persistence_file_name) as persistence:
+            report = persistence.report
+
+        assert_that(report).is_equal_to(report_object)
+
+        os.unlink(persistence_file_name)
+        os.rmdir(output)
 
     def test_with_block_with_non_existent_path(self):
         self.assertRaises(Exception, lambda: Persistence('/non/existent/path'))
