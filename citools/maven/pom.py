17629101373c2f3cda72ee7c5c944d2b268c3b2c
Andreas Würl <andi@tryb.de>
2015-09-18 07:51:55 +0200

    fixed namespace for python 2.6

--- citools/maven/pom.py
+++ citools/maven/pom.py
@@ -36,4 +36,4 @@ class Pom(object):
     @property
     def modules(self):
         return [element.text for element in
-                self.pom_tree.find('./{{{0}}}mvn-pom:modules'.format(self.NAMESPACE))]
+                self.pom_tree.find('./{{{0}}}modules'.format(self.NAMESPACE))]
